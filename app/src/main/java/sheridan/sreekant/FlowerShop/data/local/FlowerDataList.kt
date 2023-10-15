9cba9510355af5cdca2062abaf59272e634a5b30
Supritha Sreekanth <sreekant@sheridancollege.ca>
2023-10-15 16:37:06 -0400

    Added Flower object and hardcoded list of flower data

---
+++ app/src/main/java/sheridan/sreekant/FlowerShop/data/local/FlowerDataList.kt
@@ -0,0 +1,43 @@
+package sheridan.sreekant.FlowerShop.data.local
+
+import sheridan.sreekant.FlowerShop.domain.Flower
+
+val flowerList: List<Flower> = listOf(
+
+    Flower(
+        "aster",
+        "Asters",
+        2.99,
+        "The name Aster comes from the Ancient Greek word meaning star, referring to the shape of the flower head.",
+        "aster.jpeg"
+    ),
+    Flower(
+        "carnation",
+        "Carnations",
+        1.99,
+        "Carnations require well-drained, neutral to slightly alkaline soil, and full sun. Examples include Gina Porto, Helen and Red Rocket",
+        "carnation.jpeg"
+    ),
+    Flower(
+        "daffodil",
+        "Daffodils",
+        1.99,
+        "Daffodil is a common English name, sometimes used for all varieties, and commonly used by the American Daffodil Society",
+        "daffodil.jpeg"
+    ),
+    Flower(
+        "rose",
+        "Roses",
+        4.99,
+        "A rose is a woody perennial within the family Rosaceae. They form a group of erect shrubs, and climbing or trailing plants.",
+        "rose.jpeg"
+    ),
+    Flower(
+        "orchid",
+        "Orchids",
+        10.99,
+        "The orchid family is a diverse and widespread family in the order Asparagales. It is one of the largest families of flowering plants.",
+        "orchid.jpeg"
+    )
+
+)
---
+++ app/src/main/java/sheridan/sreekant/FlowerShop/domain/Flower.kt
@@ -0,0 +1,12 @@
+package sheridan.sreekant.FlowerShop.domain
+
+data class Flower(
+
+    val id: String,
+    val name: String,
+    val price: Double,
+    val description: String,
+    val image: String
+
+
+)
